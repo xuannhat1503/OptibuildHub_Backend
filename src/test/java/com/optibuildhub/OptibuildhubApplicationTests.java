90323851b30184adf1e99611483b78233604329d
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-08 02:21:25 +0700

    first

---
+++ src/test/java/com/optibuildhub/OptibuildhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optibuildhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptibuildhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
