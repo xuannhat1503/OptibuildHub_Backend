c1ac40284f5e8d5a48eae8548a5ad3acade35661
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-08 08:23:01 +0700

    update

--- src/main/java/com/optibuildhub/config/CorsConfig.java
+++ src/main/java/com/optibuildhub/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                "optibuild-hub.vercel.app",
+                "https://optibuildhub-backend.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
